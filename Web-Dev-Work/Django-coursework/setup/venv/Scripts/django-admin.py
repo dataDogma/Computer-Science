79f22eb049001258e989127196a42c7987d7f524
Rahul Yadav <rahul.yadav.donny@gmail.com>
2017-01-25 09:40:47 +0530

    Django Env setup concluded

---
+++ Web-Dev-Work/Django-coursework/setup/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pysag\documents\github\web-dev-work\django-coursework\setup\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
